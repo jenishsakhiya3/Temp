87ac48da29380829e8c2f37dfab06c7c0e72c176
Jenish Sakhiya <jenishsakhiya3@gmail.com>
2026-09-01 14:33:21 +0530

    ...

--- src/app/auth-config.ts
+++ src/app/auth-config.ts
@@ -4,7 +4,7 @@ export const msalConfig: Configuration = {
   auth: {
     clientId: '3a221580-7548-4dfa-bb1d-15ff38c6982a', // Replace with your App Registration Client ID
     authority: 'https://login.microsoftonline.com/66c5be6d-b895-4170-9fe4-1d29253008f0', // Replace with Global Tenant ID
-    redirectUri: 'http://localhost:4200', // Update this to your production URL later
+    redirectUri: 'http://globallogin-baeecpe6hrhedqan.centralus-01.azurewebsites.net', // Update this to your production URL later
   },
   cache: {
     cacheLocation: BrowserCacheLocation.SessionStorage
