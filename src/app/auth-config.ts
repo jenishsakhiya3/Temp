e0c9efc64d455d3ab8f7a0283de51755358e1fd3
Jenish Sakhiya <jenishsakhiya3@gmail.com>
2026-09-01 15:54:28 +0530

    ..

--- src/app/auth-config.ts
+++ src/app/auth-config.ts
@@ -4,7 +4,7 @@ export const msalConfig: Configuration = {
   auth: {
     clientId: '3a221580-7548-4dfa-bb1d-15ff38c6982a',
     authority: 'https://login.microsoftonline.com/66c5be6d-b895-4170-9fe4-1d29253008f0', 
-    redirectUri: 'https://globallogin-baeecpe6hrhedqan.centralus-01.azurewebsites.net', 
+    redirectUri: 'https://globallogin-baeecpe6hrhedqan.eastus-01.azurewebsites.net', 
   },
   cache: {
     cacheLocation: BrowserCacheLocation.SessionStorage
