b756107e28f3ba6d82687f6af848e75b1cbb4732
Jenish Sakhiya <jenishsakhiya3@gmail.com>
2026-09-01 14:55:09 +0530

    remove comments

--- src/app/auth-config.ts
+++ src/app/auth-config.ts
@@ -2,9 +2,9 @@ import { Configuration, BrowserCacheLocation } from '@azure/msal-browser';
 
 export const msalConfig: Configuration = {
   auth: {
-    clientId: '3a221580-7548-4dfa-bb1d-15ff38c6982a', // Replace with your App Registration Client ID
-    authority: 'https://login.microsoftonline.com/66c5be6d-b895-4170-9fe4-1d29253008f0', // Replace with Global Tenant ID
-    redirectUri: 'https://globallogin-baeecpe6hrhedqan.centralus-01.azurewebsites.net', // Update this to your production URL later
+    clientId: '3a221580-7548-4dfa-bb1d-15ff38c6982a',
+    authority: 'https://login.microsoftonline.com/66c5be6d-b895-4170-9fe4-1d29253008f0', 
+    redirectUri: 'https://globallogin-baeecpe6hrhedqan.centralus-01.azurewebsites.net', 
   },
   cache: {
     cacheLocation: BrowserCacheLocation.SessionStorage
