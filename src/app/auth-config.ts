330bfa73844ef98c70ceaca7881fb28182f95e02
Jenish Sakhiya <jenishsakhiya3@gmail.com>
2026-09-07 14:16:15 +0530

    ...

--- src/app/auth-config.ts
+++ src/app/auth-config.ts
@@ -2,8 +2,8 @@ import { Configuration, BrowserCacheLocation } from '@azure/msal-browser';
 
 export const msalConfig: Configuration = {
   auth: {
-    clientId: '3c37b98b-69df-4f28-b6f5-03ad9d7ad852',
-    authority: 'https://login.microsoftonline.com/66c5be6d-b895-4170-9fe4-1d29253008f0', 
+    clientId: 'bd9afa2d-f5d5-4e67-9bc2-47e435146733',
+    authority: 'https://login.microsoftonline.com/db85f625-db9c-40ca-9289-4dbbaa596f22', 
     redirectUri: typeof window !== 'undefined' 
       ? window.location.origin 
       : 'https://globallogin-baeecpe6hrhedqan.westus3-01.azurewebsites.net', 
@@ -23,7 +23,7 @@ export const TENANTS: TenantConfig[] = [
   {
     id: 'global-tenant',
     name: 'Region1',
-    appServiceUrl: 'https://fe1-hharfzf5b6dve0dw.eastasia-01.azurewebsites.net  '
+    appServiceUrl: 'https://fe1-hharfzf5b6dve0dw.eastasia-01.azurewebsites.net'
   },
   {
     id: 'child-tenant-b',
